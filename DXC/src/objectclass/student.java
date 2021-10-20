c3998e77d75b66b7642f33733a9f9b1440bda845
hp <hp@DESKTOP-DVIVN2R>
2021-10-20 10:29:24 +0530

    objectclass

---
+++ DXC/src/objectclass/student.java
@@ -0,0 +1,20 @@
+package objectclass;
+
+public class student {
+	String name;
+	int age;
+	static String COLLEGE_Name = "NIT";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
