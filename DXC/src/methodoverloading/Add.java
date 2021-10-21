5c719367ef476433e03445c65b30f80152fb20fe
hp <hp@DESKTOP-DVIVN2R>
2021-10-21 10:15:58 +0530

    Methodoverloading and Methodoverriding

---
+++ DXC/src/methodoverloading/Add.java
@@ -0,0 +1,16 @@
+package methodoverloading;
+
+public class Add {
+	static int add(int a,int b){
+		return a+b;
+		}  
+	static int add(int a,int b,int c){
+		return a+b+c;
+		}  
+	}  
+	class TestOverloading1{  
+	public static void main(String[] args){  
+	System.out.println(Add.add(25,66));  
+	System.out.println(Add.add(76,11,199));  
+	}
+}
---
+++ DXC/src/methodoverriding/vehicle.java
@@ -0,0 +1,16 @@
+package methodoverriding;
+
+public class vehicle {
+	void run(){
+		System.out.println("Vehicle is running");
+		}
+}
+class Bike2 extends vehicle{
+	void run(){
+		System.out.println("vehicle is not running");
+	}
+	public static void main(String args[]){
+		Bike2 obj = new Bike2();  
+		obj.run();
+	}
+}
