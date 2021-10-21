ec7094430d18b8f60433a1d1ccbfafcca681a91e
hp <hp@DESKTOP-DVIVN2R>
2021-10-21 10:52:52 +0530

    Constructors

---
+++ DXC/src/objectclass/Debug.java
@@ -0,0 +1,19 @@
+package objectclass;
+import basics.Calculator;
+
+public class Debug {
+
+		public static void main(String[] args) {
+			
+			Calculator cal = new Calculator();
+			System.out.println("Debug-- main");
+			int sum = cal.add(10, 20);
+			System.out.println("The sum is--" +sum);
+			student Nymisha = new student("Nymisha",456);
+			student mystudent = new student();
+			student yourstudent = new student("anjali");
+			for(int a=0; a<5; a++) {
+				int f =a+5 *3;
+			}
+		}
+}
