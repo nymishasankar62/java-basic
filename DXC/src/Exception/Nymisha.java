902aacb08cec7d80e586c6b3fdd16192ef8fc094
nymishasankar62 <92718747+nymishasankar62@users.noreply.github.com>
2021-10-22 17:36:16 +0530

    Delete Nymisha.java

--- DXC/src/Exception/Nymisha.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-public class Nymisha {
-	public static void main(String[] args) {
-		abstraction.email.Nymisha nymisha = new abstraction.email.Nymisha();
-		Nymisha nymisha2 = new Nymisha();
-	}
-
-}
