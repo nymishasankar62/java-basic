9e3ad17aa04a32a685f83effb4661fdeee4acfe4
hp <hp@DESKTOP-DVIVN2R>
2021-10-27 15:11:57 +0530

    Test

--- DXC/src/Abstract/BmwFlyingCar.java
+++ DXC/src/Abstract/BmwFlyingCar.java
@@ -10,4 +10,12 @@ public  class BmwFlyingCar extends Flyingcar {
 	@Override
 	public void landcar() {
 	}
+	public static	int add(int a, int b) {
+		return a+b;
+	}
+
+	public static boolean isEligibletoVote(int age) {
+		if (age >17) {return true;}
+		return false;
+	}
 }
---
+++ DXC/src/Abstract/BmwTest.java
@@ -0,0 +1,24 @@
+package Abstract;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class BmwTest extends TestCase {
+
+		@Test
+		public void testAdd() {
+			int expected = 40;
+			int actual = BmwFlyingCar.add(10, 20);
+			//assertEquals(expected, actual);
+		}
+
+		@Test
+		public void testElgibility() {
+			//boolean expected = false;
+			boolean actual = BmwFlyingCar.isEligibletoVote(18);
+
+			//assertFalse(actual);
+			assertTrue(actual);
+		}
+
+}
