8c81df8d2be752777d5d822e0a8371bfbdaa0452
hp <hp@DESKTOP-DVIVN2R>
2021-10-27 15:20:07 +0530

    polymorphism

---
+++ DXC/src/Polymorphism/Animal.java
@@ -0,0 +1,11 @@
+package Polymorphism;
+
+public class Animal {
+	Animal(){System.out.println("animal is created");}  
+	}  
+	class Dog extends Animal{  
+		Dog(){  
+			System.out.println("dog is created");  
+		}  
+
+}
---
+++ DXC/src/Polymorphism/Testsuper.java
@@ -0,0 +1,8 @@
+package Polymorphism;
+
+public class Testsuper {
+	public static void main(String args[]){  
+		Dog d=new Dog();  
+	}
+
+}
